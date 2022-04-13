f15082e02f2a7265c27b53d26bba627afce00fd3
Susan Choi <2539lee@gmail.com>
2022-04-12 23:45:50 -0700

    Blog_setup & utilize state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+/*eslint-disable*/
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  let [postTitle, postEdit] = useState([
+    'Parks for kids',
+    'Best coffee in town',
+    'Japanese restaurant in Torrance',
+  ])
+  let [likeCount, likeEdit] = useState(0)
+
+  let posts = 'OC Korean BBQ'
+
+  function changeTitle() {
+    let newArray = [...postTitle]
+    newArray[0] = 'OC Peruvian'
+    postEdit(newArray)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='black-nav'>
+        <div>Developer BLOG</div>
+      </div>
+      <div className='list'>
+        <h3>{posts}</h3>
+        <p>posted on April 12</p>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3>
+          {postTitle[0]}{' '}
+          <span
+            onClick={() => {
+              likeEdit(likeCount + 1)
+            }}
+          >
+            👍🏼
+          </span>
+          {likeCount}
+        </h3>
+        <p>posted on April 12</p>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3>{postTitle[1]}</h3>
+        <p>posted on April 13</p>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3>{postTitle[2]}</h3>
+        <p>posted on April 13</p>
+        <hr />
+      </div>
+
+      <button onClick={changeTitle}>Change title</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
