2cb2a04c8c6ec7a333778ffd7d5bef35ba2fc9a7
Susan Choi <2539lee@gmail.com>
2022-04-15 23:59:21 -0700

    Modal

--- src/App.js
+++ src/App.js
@@ -10,30 +10,31 @@ function App() {
     'Japanese restaurant in Torrance',
   ])
   let [likeCount, likeEdit] = useState(0)
+  let [modal, modalEdit] = useState(false)
 
   let posts = 'OC Korean BBQ'
 
-  function changeTitle() {
-    let newArray = [...postTitle]
-    newArray[0] = 'OC Peruvian'
-    postEdit(newArray)
-  }
+  // function changeTitle() {
+  //   let newArray = [...postTitle]
+  //   newArray[0] = 'OC Peruvian'
+  //   postEdit(newArray)
+  // }
 
-  function sortTitle() {
-    let newArray1 = [...postTitle].sort()
-    postEdit(newArray1)
-  }
+  // function sortTitle() {
+  //   let newArray1 = [...postTitle].sort()
+  //   postEdit(newArray1)
+  // }
 
   return (
     <div className='App'>
       <div className='black-nav'>
         <div>Developer BLOG</div>
       </div>
-      <div className='list'>
+      {/* <div className='list'>
         <h3>{posts}</h3>
         <p>posted on April 12</p>
         <hr />
-      </div>
+      </div> */}
       <div className='list'>
         <h3>
           {postTitle[0]}{' '}
@@ -60,13 +61,35 @@ function App() {
         <hr />
       </div>
 
-      <div>
+      {/* <div>
         <button onClick={changeTitle}>Change title</button>
       </div>
 
       <div>
         <button onClick={sortTitle}>Sort title</button>
+      </div> */}
+
+      <div>
+        <button
+          onClick={() => {
+            modalEdit(!modal)
+          }}
+        >
+          Toggle
+        </button>
+        {modal === true ? <Modal /> : null}
       </div>
+    </div>
+  )
+}
+
+// Component
+function Modal() {
+  return (
+    <div className='modal'>
+      <h2>Title</h2>
+      <p>Date</p>
+      <p>Contents</p>
     </div>
   )
 }
